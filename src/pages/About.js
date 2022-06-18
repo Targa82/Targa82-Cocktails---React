206ae4a3b0926ee8bdab80963ae871c7f4e9f0f6
Targa82 <101151467+Targa82@users.noreply.github.com>
2022-06-18 11:12:15 +0200

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+        cupiditate ut ratione, mollitia repellendus fugit similique ex totam
+        voluptates saepe ea facere, laboriosam voluptas sunt nobis quo commodi,
+        quibusdam soluta.
+      </p>
+    </section>
+  );
+};
+
+export default About;
